68622479c9d20aeff4a0fb8f656bc27435df4ff6
Utkarsh dubey <dubeyutkarsh2000@gmail.com>
2021-05-12 20:55:02 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from bot import telegram_chatbot 
-# from urllib.request import Request, urlopen
-from urllib2 import urlopen
-from urllib2 import Request
+from urllib.request import Request, urlopen
+# from urllib2 import urlopen
+# from urllib2 import Request
 import json
 from time import sleep
 bot=telegram_chatbot("config.cfg")
